7fa39015b9ba11f4c832f5245bf7bb4de57cd03e
franvi07 <fvinet07@gmail.com>
2022-12-12 15:22:00 +0100

    change propagators for estimation (numerical propagator)

--- src/Kalman/brouillon_Kalman.java
+++ src/Kalman/brouillon_Kalman.java
@@ -1,51 +1,53 @@
 package src.Kalman;
+
 import java.io.File;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.SortedSet;
 
 import org.hipparchus.linear.DiagonalMatrix;
+import org.hipparchus.linear.QRDecomposer;
 import org.hipparchus.linear.RealMatrix;
 import org.hipparchus.ode.events.Action;
+import org.hipparchus.optim.nonlinear.vector.leastsquares.GaussNewtonOptimizer;
+import org.hipparchus.optim.nonlinear.vector.leastsquares.LeastSquaresOptimizer;
 import org.hipparchus.random.CorrelatedRandomVectorGenerator;
-import org.orekit.bodies.CelestialBody;
+import org.hipparchus.random.GaussianRandomGenerator;
+import org.hipparchus.random.RandomDataGenerator;
+import org.hipparchus.random.RandomGenerator;
 import org.orekit.bodies.GeodeticPoint;
 import org.orekit.bodies.OneAxisEllipsoid;
 import org.orekit.data.DataContext;
 import org.orekit.data.DataProvidersManager;
 import org.orekit.data.DirectoryCrawler;
+import org.orekit.estimation.leastsquares.BatchLSEstimator;
 import org.orekit.estimation.measurements.GroundStation;
 import org.orekit.estimation.measurements.ObservableSatellite;
 import org.orekit.estimation.measurements.ObservedMeasurement;
 import org.orekit.estimation.measurements.generation.AngularAzElBuilder;
 import org.orekit.estimation.measurements.generation.EventBasedScheduler;
 import org.orekit.estimation.measurements.generation.Generator;
-import org.orekit.estimation.measurements.generation.MeasurementBuilder;
 import org.orekit.estimation.measurements.generation.SignSemantic;
 import org.orekit.estimation.sequential.ConstantProcessNoise;
-import org.orekit.estimation.sequential.CovarianceMatrixProvider;
 import org.orekit.estimation.sequential.KalmanEstimator;
 import org.orekit.estimation.sequential.KalmanEstimatorBuilder;
-import org.orekit.estimation.sequential.KalmanModel;
 import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
 import org.orekit.frames.ITRFVersion;
 import org.orekit.frames.TopocentricFrame;
 import org.orekit.models.AtmosphericRefractionModel;
+import org.orekit.orbits.CartesianOrbit;
 import org.orekit.orbits.KeplerianOrbit;
+import org.orekit.orbits.Orbit;
 import org.orekit.orbits.PositionAngle;
 import org.orekit.propagation.Propagator;
-import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.analytical.KeplerianPropagator;
+import org.orekit.propagation.conversion.DormandPrince853IntegratorBuilder;
 import org.orekit.propagation.conversion.KeplerianPropagatorBuilder;
-import org.orekit.propagation.conversion.OrbitDeterminationPropagatorBuilder;
+import org.orekit.propagation.conversion.NumericalPropagatorBuilder;
 import org.orekit.propagation.events.BooleanDetector;
 import org.orekit.propagation.events.ElevationDetector;
 import org.orekit.propagation.events.GroundAtNightDetector;
-import org.orekit.propagation.events.handlers.ContinueOnEvent;
-import org.orekit.propagation.events.handlers.RecordAndContinue;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.FixedStepSelector;
 import org.orekit.time.TimeScale;
@@ -53,13 +55,13 @@ import org.orekit.time.TimeScalesFactory;
 import org.orekit.utils.Constants;
 import org.orekit.utils.IERSConventions;
 import org.orekit.utils.PVCoordinatesProvider;
-import org.orekit.utils.ParameterDriversList;
 import org.orekit.bodies.CelestialBodyFactory;
 
 public class brouillon_Kalman {
-	
+
     public static void main(String[] args) throws Exception
     {
+    	
     	//importation des donnees de base (toujour mettre ça en début de programme)
     	File orekitData = new File("lib/orekit-data-master");
     	DataProvidersManager manager = DataContext.getDefault().getDataProvidersManager();
@@ -73,6 +75,7 @@ public class brouillon_Kalman {
     	double raan = 0.2;
     	double anomaly = 1;
     	PositionAngle type = PositionAngle.TRUE;
+    	double[] listParamOrbitaux = {a,e,i,pa,raan,anomaly};
 
     	final TimeScale utc = TimeScalesFactory.getUTC();
     	AbsoluteDate initialDate = new AbsoluteDate(2014, 6, 25, utc);
@@ -83,7 +86,7 @@ public class brouillon_Kalman {
     	final double t = initialOrbit.getKeplerianPeriod();
     	final AbsoluteDate finalDate = initialDate.shiftedBy(t * 30.); // on propagera sur 30 orbites
     	
-    	ObservableSatellite satellite = new ObservableSatellite(0); //à voir pour le 0...
+    	ObservableSatellite satellite = new ObservableSatellite(0);
     	
     	//propagateur
     	final KeplerianPropagator propagator = new KeplerianPropagator(initialOrbit);
@@ -101,11 +104,16 @@ public class brouillon_Kalman {
     	GroundStation station1 = new GroundStation(topocentricFrame);
     	    	
     	//Mesures
-    	CorrelatedRandomVectorGenerator noiseSource = null;//mesures parfaites
-    	double[] sigma = {2.,2.};//J'ai l'impression que ça a aucun effet
-    	double[] baseWeight = {1.,1.};
-    	AngularAzElBuilder mesuresBuilder = new AngularAzElBuilder(noiseSource, station1, sigma, baseWeight, satellite);
-    	FixedStepSelector dateSelector = new FixedStepSelector(600,utc); // une mesure toutes les 10min
+    	double[] mean = {0.,0.};//pas de biais
+    	double[] covarianceDiag = {0.3*3.14/180,0.3*3.14/180};//incertitude de mesure : 0.3°
+    	RealMatrix covariance = new DiagonalMatrix(covarianceDiag);
+    	RandomGenerator randomGenerator = new RandomDataGenerator();
+    	GaussianRandomGenerator gaussianRandomGenerator = new GaussianRandomGenerator(randomGenerator);
+    	CorrelatedRandomVectorGenerator noiseSource = new CorrelatedRandomVectorGenerator(mean, covariance, 0, gaussianRandomGenerator);//mesures parfaites:null
+    	double[] sigma = {1,1};//covarianceDiag;//ca sert juste à normaliser (?)
+    	double[] baseWeight = {1,1};
+    	AngularAzElBuilder mesuresBuilder = new AngularAzElBuilder(noiseSource, station1, sigma, baseWeight, satellite); // Julie aura besoin d'un objet satellite
+    	FixedStepSelector dateSelector = new FixedStepSelector(600, utc); // une mesure toutes les 10min
     	//Detecteur d'elevation (quid azimuth?)
     	ElevationDetector elevationDetector = new ElevationDetector(topocentricFrame);
     	elevationDetector = elevationDetector.withHandler(
@@ -129,6 +137,7 @@ public class brouillon_Kalman {
                     );
                     return increasing ? Action.CONTINUE : Action.CONTINUE;
                 });
+
     	BooleanDetector finalDetector = BooleanDetector.andCombine(elevationDetector, nightDetector);
     	EventBasedScheduler scheduler = new EventBasedScheduler(mesuresBuilder, dateSelector, propagator, finalDetector, SignSemantic.FEASIBLE_MEASUREMENT_WHEN_POSITIVE);
 
@@ -137,26 +146,98 @@ public class brouillon_Kalman {
     	generator.addPropagator(propagator);
     	generator.addScheduler(scheduler);
     	SortedSet<ObservedMeasurement<?>> list_measurement = generator.generate(initialDate, finalDate);
-    	for(ObservedMeasurement<?> mesure : list_measurement) {
-    		System.out.println(Arrays.toString(mesure.getObservedValue()));
-    		System.out.println(mesure.getDate());
+    	for(ObservedMeasurement<?> measure : list_measurement) {
+    		System.out.println(Arrays.toString(measure.getObservedValue()));
+    		System.out.println(measure.getDate());
     	}
+
+    	// orbite initiale prédite ?
+    	/*
+    	double ai = a+1000;
+    	double ei = e+0.01;
+    	double ii = i+0.01;
+    	double pai = pa+0.01;
+    	double raani = raan+0.01;
+    	double anomalyi = anomaly+0.01;
+    	*/
+
+    	double ai = a*1.001;
+    	double ei = e*1.001;
+    	double ii = i*1.001;
+    	double pai = pa*1.001;
+    	double raani = raan*1.001;
+    	double anomalyi = anomaly*1.001;
+
+    	/*
+    	//BLS ne converge pas avec ça mais Kalman si:
+    	double ai = a*1.01;
+    	double ei = e*1.01;
+    	double ii = i*1.01;
+    	double pai = pa*1.01;
+    	double raani = raan*1.01;
+    	double anomalyi = anomaly*1.01;
+    	*/
+    	AbsoluteDate initialDatei = new AbsoluteDate(2014, 6, 25, utc);
+    	KeplerianOrbit initialOrbiti = new KeplerianOrbit(ai, ei, ii, pai, raani, anomalyi, type, frame, initialDatei, Constants.EGM96_EARTH_MU);
+    	CartesianOrbit initialOrbitiC =new CartesianOrbit(initialOrbiti);
     	
-    	KalmanEstimatorBuilder estimatorBuilder = new KalmanEstimatorBuilder();
-    	// initial covariance matrix 
-    	double[] ip = {0.,0.,0.,0.,0.,0.};
+    	// Propagateur au choix :
+    	// numérique :
+    	double prop_min_step = 0.001;// # s
+    	double prop_max_step = 300.0;// # s
+    	double prop_position_error = 10.0;// # m
+    	DormandPrince853IntegratorBuilder integratorBuilder = new DormandPrince853IntegratorBuilder(prop_min_step, prop_max_step, prop_position_error);
+    	NumericalPropagatorBuilder numericalPropagatorBuilder = new NumericalPropagatorBuilder(initialOrbitiC, integratorBuilder, type, 1.0);
+    	// ou Képlerien :
+    	KeplerianPropagatorBuilder keplerianPropagatorBuilder = new KeplerianPropagatorBuilder(initialOrbiti, type, 1.0); //initialOrbit : reference orbit from which real orbits will be built
+
+    	//BLS method
+    	QRDecomposer matrixDecomposer = new QRDecomposer(1e-11);
+    	LeastSquaresOptimizer optimizer = new GaussNewtonOptimizer(matrixDecomposer, false);
+    	BatchLSEstimator BLSEstimator = new BatchLSEstimator(optimizer, numericalPropagatorBuilder);
+    	for(ObservedMeasurement<?> measure : list_measurement) {
+    		BLSEstimator.addMeasurement(measure);
+    	}
+    	double estimator_convergence_thres = 1e-3;
+    	int estimator_max_iterations = 25;
+    	int estimator_max_evaluations = 35;
+    	BLSEstimator.setParametersConvergenceThreshold(estimator_convergence_thres);
+    	BLSEstimator.setMaxIterations(estimator_max_iterations);
+    	BLSEstimator.setMaxEvaluations(estimator_max_evaluations);
+
+    	Propagator[] propagatorEstimated_LSE = BLSEstimator.estimate();
+    	//Estimation des params
+    	System.out.println("Batch Least Square Method");
+    	System.out.println(Arrays.toString(paramOrbitaux(propagatorEstimated_LSE[0].getInitialState().getOrbit())));
+    	System.out.println(Arrays.toString(listParamOrbitaux));
+    	//RealMatrix physicalCovariance = BLSEstimator.getPhysicalCovariances(0);
+
+    	// Kalman method
+
+    	// initial covariance matrix
+    	//double[] ip = {1e-14,1e-14,1e-14,1e-14,1e-14,1e-14};
+    	double[] ip = {2000,0.02,0.02,0.02,0.02,0.02};
     	RealMatrix initialP = new DiagonalMatrix(ip);
     	// process noise matrix 
-    	double[] q = {0.,0.,0.,0.,0.,0.};
+    	double[] q = {1e-13,1e-13,1e-13,1e-13,1e-13,1e-13};
     	RealMatrix Q = new DiagonalMatrix(q);
     	
+    	System.out.println("Kalman Method");
+    	KalmanEstimatorBuilder estimatorBuilder = new KalmanEstimatorBuilder();
     	ConstantProcessNoise processNoise = new ConstantProcessNoise(initialP, Q);
-    	KeplerianPropagatorBuilder keplerianPropagatorBuilder = new KeplerianPropagatorBuilder(initialOrbit, type, 1.); //initialOrbit : reference orbit from which real orbits will be built
-    	estimatorBuilder.addPropagationConfiguration(keplerianPropagatorBuilder, processNoise);
+    	estimatorBuilder.addPropagationConfiguration(numericalPropagatorBuilder, processNoise);
     	KalmanEstimator kalmanEstimator = estimatorBuilder.build();
-    	Propagator[] propagatorEstimated = kalmanEstimator.processMeasurements(list_measurement);
-    	//Estimation du paramètre i (seulement)
-    	System.out.println(propagatorEstimated[0].getInitialState().getOrbit().getI());
-    }	
+    	Propagator[] propagatorEstimated_Kalman = kalmanEstimator.processMeasurements(list_measurement);
+    	//Estimation des params
+    	System.out.println(Arrays.toString(paramOrbitaux(propagatorEstimated_Kalman[0].getInitialState().getOrbit())));
+    	System.out.println(Arrays.toString(listParamOrbitaux));        
+    	// comment il crée la matrice de covariance pour les mesures dans le KALMAN ?
+    	// comprendre P et Q
+    }
 
+    static double[] paramOrbitaux(Orbit orbit){
+    	KeplerianOrbit nOrbit = new KeplerianOrbit(orbit);
+		double[] param = {nOrbit.getA(), nOrbit.getE(), nOrbit.getI(), nOrbit.getRightAscensionOfAscendingNode(), nOrbit.getPerigeeArgument()};
+		return param;
+    }
 }
