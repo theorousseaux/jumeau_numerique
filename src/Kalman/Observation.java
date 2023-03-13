3ffd948f7d3a4a640fe5d49f350c7cfd019dac61
franvi07 <fvinet07@gmail.com>
2023-03-13 10:54:16 +0100

    correction Observation pour prendre en compte les couples mesures/vraisetats

--- src/Kalman/Observation.java
+++ src/Kalman/Observation.java
@@ -7,12 +7,14 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+import org.hipparchus.util.Pair;
 import org.orekit.estimation.measurements.AngularAzEl;
 import org.orekit.estimation.measurements.ObservableSatellite;
 import org.orekit.estimation.measurements.ObservedMeasurement;
 import org.orekit.estimation.measurements.generation.EventBasedScheduler;
 import org.orekit.estimation.measurements.generation.Generator;
 import org.orekit.propagation.Propagator;
+import org.orekit.propagation.SpacecraftState;
 import org.orekit.time.AbsoluteDate;
 
 public class Observation {
@@ -31,21 +33,23 @@ public class Observation {
 		this.finalDate = finalDate;
 	}
 	
-	public List<SortedSet<ObservedMeasurement<?>>> measure(boolean plot){
-		Generator generator = new Generator();
+	public Pair<List<SortedSet<ObservedMeasurement<?>>>,List<List<SpacecraftState>>> measure(boolean plot){
+		CustomGenerator generator = new CustomGenerator();
 		for(int i = 0; i < this.objectsList.size(); i++) {
 			generator.addPropagator(propagatorsList.get(i));
 		}
 	    for(TelescopeAzEl telescope : this.telescopesList) {
 	    	for(int i = 0; i < this.objectsList.size(); i++) {
 				System.out.println("considering object : " + i);
-	    		EventBasedScheduler scheduler = telescope.createEventBasedScheduler(this.objectsList.get(i), this.propagatorsList.get(i));
+	    		CustomEventBasedScheduler scheduler = telescope.createCustomEventBasedScheduler(this.objectsList.get(i), this.propagatorsList.get(i));
 	        	generator.addScheduler(scheduler);
 	        	//generator.addPropagator(propagatorsList.get(i));
 	        }
 	    }
 		System.out.println("generating measurements : ");
-	    SortedSet<ObservedMeasurement<?>> measurementsList = generator.generate(this.initialDate, this.finalDate);
+	    Pair<SortedSet<ObservedMeasurement<?>>, List<SpacecraftState>> pair = generator.generate(this.initialDate, this.finalDate);
+	    SortedSet<ObservedMeasurement<?>> measurementsList = pair.getFirst();
+	    List<SpacecraftState> trueStatesList = pair.getSecond();
 		System.out.println("done");
 	    
 	    if(plot) {
@@ -62,16 +66,21 @@ public class Observation {
 	    	System.out.println("END MESURES");
 	    }
 	    
+	    List<List<SpacecraftState>> trueStatesSetsList = new ArrayList<List<SpacecraftState>>();
 	    List<SortedSet<ObservedMeasurement<?>>> measurementsSetsList = new ArrayList<SortedSet<ObservedMeasurement<?>>>();
 	    for (int i = 0; i < propagatorsList.size(); i++) {
 	    	measurementsSetsList.add(new TreeSet<ObservedMeasurement<?>>());
+	    	trueStatesSetsList.add(new ArrayList<SpacecraftState>());
 	    }
-	    
+        
+        int i = 0;
 	    for(ObservedMeasurement<?> measure : measurementsList){
 	    	measurementsSetsList.get(measure.getSatellites().get(0).getPropagatorIndex()).add(measure);
+	    	trueStatesSetsList.get(measure.getSatellites().get(0).getPropagatorIndex()).add(trueStatesList.get(i));
+	    	i = i+1;
 	    }
 	    
-	    return measurementsSetsList;
+	    return new Pair<List<SortedSet<ObservedMeasurement<?>>>,List<List<SpacecraftState>>>(measurementsSetsList, trueStatesSetsList);
 	}
 
 }
